be8add4811a15b3de8f9b46231c8a42d9061789b
Reynald <reynaldintelligence@gmail.com>
2025-05-16 20:11:53 +0700

    Fix Procfile, remove config.py, add gitattributes

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Memuat variabel dari file .env ke dalam environment
-load_dotenv()
-
-# Mengambil nilai API key dari environment
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
